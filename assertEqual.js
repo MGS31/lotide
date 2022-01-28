c8a43453a0fe19a1ecaf6081f445ff4ad86b7efe
Matt <mgstankov30@gmail.com>
2022-01-27 20:21:14 -0500

    refactoring the code to make it more streamlined

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 const assertEqual = (actual, expected) => {
- return actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
- : console.log(`⚠️ ⚠️ ⚠️  Assertion Failed: ${actual} !== ${expected} `);
+ return actual === expected ? console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`)
+ : console.log(`⚠️ ⚠️ ⚠️  Assertion Failed: ${actual} !== ${expected}`);
 };
 
 assertEqual("lighthouse labs", "Bootcamp");
