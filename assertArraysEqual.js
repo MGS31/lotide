56702862188b3ef115decf390773b1713674b3d7
Matt <mgstankov30@gmail.com>
2022-01-28 16:14:48 -0500

    assertArraysequal temp completion as I want a pier review before pushing to master

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+const eqArrays = (actual, expected) => {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i] || actual.length !== expected.length) {
+      return console.log(`⚠️ ⚠️ ⚠️  Assertion Failed: ${actual} !== ${expected}`);
+    }
+  }
+  return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+};
+
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));// => false
