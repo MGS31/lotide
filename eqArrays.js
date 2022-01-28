b2030e070dbbb1c3383785ec76ae1efd8ff91b4c
Matt <mgstankov30@gmail.com>
2022-01-28 15:51:57 -0500

    Completed code for the eqArrays task

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = (actual, expected) => {
+  return actual === expected ? console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`)
+  : console.log(`⚠️ ⚠️ ⚠️  Assertion Failed: ${actual} !== ${expected}`);
+ };
+
+ const eqArrays = (actual, expected) => {
+   let isEqual = true;
+   for (let i = 0; i < actual.length; i++) {
+     if (actual[i] !== expected[i] || actual.length !== expected.length) {
+       isEqual = false;
+       break;
+     }
+   }
+   return isEqual;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));// => false
+
+//const eqArrays = (actual, expected) => {
+//  actual.forEach((num1, index) => {
+//    const num2 = expected[index];
+//    console.log(num1, num2);
+//  })
+//}
