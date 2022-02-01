e73362da4b5af8f3c75b55c0d46351bc40af66a3
Matt <mgstankov30@gmail.com>
2022-02-01 11:19:20 -0500

    adding the final edits for the countOnly code

--- countOnly.js
+++ countOnly.js
@@ -10,14 +10,14 @@ const countOnly = (allItems, itemsToCount) => {
   const results = {};
 
   for (const item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-      console.log("Test 1", results[item]);
-    } else {
-      results[item] = 1;
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   }
-
   return results;
 };
 
@@ -34,11 +34,12 @@ const firstNames = [
   "Joe"
 ];
 
-const result1 = countOnly(firstNames, {"Jason" : true, "Karima": true, "Fang" : true, "Agouhanna" : false });
+//const result1 = countOnly(firstNames, {"Jason" : true, "Karima": true, "Fang" : true, "Agouhanna" : false });
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
 console.log(result1);
 
-//assertEqual(result1["Jason"], 1);
-//assertEqual(result1["Karima"], undefined);
-//assertEqual(result1["Fang"], 2);
-//assertEqual(result1["Agouhanna"], undefined);
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
