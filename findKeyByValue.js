b1c42a5bf0a6821ccfd79fe093d87469ed70a4aa
Matt <mgstankov30@gmail.com>
2022-02-01 22:00:51 -0500

    small edit to the function

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -33,4 +33,4 @@ console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);//
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
