27486bbea47489bd5f99c50f6ba26a2618227458
Matt <mgstankov30@gmail.com>
2022-02-15 10:36:11 -0500

    adding the tests for assertEquals and Head to ensure they are working properly when needed

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("lighthouse labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("lighthouse labs", "lighthouse labs");
+assertEqual("Hello", "Hi");
+assertEqual(2022, 2022);
+assertEqual(1,6);
+assertEqual([1,2,3],[1,2,3]);
+assertEqual([1,2],[1,2,3,]);
