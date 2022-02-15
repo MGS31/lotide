67ac1b3af75d0538aa1240896cace78ef83cafdc
Matt <mgstankov30@gmail.com>
2022-02-15 10:41:35 -0500

    refactoring the tail code to use module exports with separated test code

--- tail.js
+++ tail.js
@@ -1,7 +1,4 @@
-const assertEqual = (actual, expected) => {
-  return actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`⚠️ ⚠️ ⚠️  Assertion Failed: ${actual} !== ${expected} `);
-};
+const assertEqual = require('./assertEqual');
 
 const tail = (array) => {
   // simple variable checking for the array slicing the elements from the first index onward.
@@ -11,18 +8,5 @@ const tail = (array) => {
   return sum;
 };
 
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result[0], ["Lighthouse", "Labs"][0]);
-assertEqual(result[1], ["Lighthouse", "Labs"][1]);
-assertEqual(result.length, 2);
-
-const resultTo = tail([3, 2, 1]);
-assertEqual(resultTo[0], [2, 1][0]);
-assertEqual(resultTo[1], [2, 1][1]);
-
-const words = ["Yo Yo", "Lighthouse", "labs"];
-tail(words);
-assertEqual(words.length, 3);
-
+module.exports = tail;
 // adding a refernce to the help from @cangoman who assisted as a mentor on this!
