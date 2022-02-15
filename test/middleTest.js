590ea7c15a95f99e36b1c6823ff6b42a3a79cc49
Matt <mgstankov30@gmail.com>
2022-02-15 12:10:24 -0500

    refactoring the middle test file to use mocha and chai type testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,23 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [4]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 3]);
-assertArraysEqual(middle([2]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+describe('#Middle', () => {
+  it("returns [2] for [1,2,3]", () => {
+    assert.deepEqual(middle([1,2,3,]), [2]);
+  });
+  it("returns [4[ for [1,2,3,4,5]", () => {
+    assert.notDeepEqual(middle([1,2,3,4,5]), [4]);
+  });
+  it("returns [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+  it("returns [3,3] for [1,2,3,4,5,6]", () => {
+    assert.notDeepEqual(middle([1,2,3,4,5,6]), [3,3]);
+  });
+  it("returns [] for [2]", () => {
+    assert.notDeepEqual(middle([2]), [2]);
+  });
+  it("returns [4,5] for [1,2,3,4,5,6,7,8]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+  });
+});
