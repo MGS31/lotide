8f49e901d3f45bd8acff1be45b5cec810c069341
Matt <mgstankov30@gmail.com>
2022-02-15 11:30:07 -0500

    Updating the headTest file to use mocha and chai automated testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,28 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([6,6,7]), 5);
-assertEqual(head([]), 5);
-assertEqual(head([7]), 5);
+const assert = require('chai').assert;
+
+describe("#Head", () => {
+  it("returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1,2,3]), 1);
+  });
+  it("retruns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']),'5');
+  });
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7,]),5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+  it("returns 5 for [6,6,7]", () => {
+    assert.notStrictEqual(head([6,6,7]), 5);
+  });
+  it("returns 5 for []", () => {
+    assert.notStrictEqual(head([]), 5);
+  });
+  it("returns 7 for [5]", () => {
+    assert.notStrictEqual(head([5]),7);
+  });
+});
