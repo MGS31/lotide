4e4fd3166a4a16ae663a862da886bcc4690e75df
Matt <mgstankov30@gmail.com>
2022-02-15 10:35:02 -0500

    refactoring the code to use the module exports

--- assertEqual.js
+++ assertEqual.js
@@ -6,11 +6,5 @@ const assertEqual = (actual, expected) => {
     : console.log(`⚠️ ⚠️ ⚠️  Assertion Failed: ${actual} !== ${expected}`);
 };
 
-assertEqual("lighthouse labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("lighthouse labs", "lighthouse labs");
-assertEqual("Hello", "Hi");
-assertEqual(2022, 2022);
-assertEqual(1,6);
-assertEqual([1,2,3],[1,2,3]);
-assertEqual([1,2],[1,2,3,]);
+module.exports = assertEqual;
+
