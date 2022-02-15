e10818de40aa2362ba9656963c8a859530694431
Matt <mgstankov30@gmail.com>
2022-02-15 12:42:18 -0500

    adjusting assert Objects equals code to allow the module exports ability

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -52,6 +52,8 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
+module.exports = assertObjectsEqual;
+
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const cd = { c: "2", d: "3" };
