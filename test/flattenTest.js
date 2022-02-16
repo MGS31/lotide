f4ab9ec134c94191d4609980322212c554e7dde6
Matt <mgstankov30@gmail.com>
2022-02-16 14:54:37 -0500

    adding additional formating to ensure cleaner code

--- test/flattenTest.js
+++ test/flattenTest.js
@@ -2,4 +2,4 @@ const assertArraysEqual = require('../assertArraysEqual');
 const flatten = require('../flatten');
 
 flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); true;
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); //true
