379b1cc75f755894bb6621eea4c8abd35a117ef7
Matt <mgstankov30@gmail.com>
2022-02-18 23:23:52 -0500

    update the test file for withoutTest to accomdate more diverse tests

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -3,6 +3,8 @@ const without = require('../without');
 
 without([1, 2, 3], [1]); // => [2, 3]
 without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+assertArraysEqual(without(["1", "1", "1", "2", "3", "1" , "1"], ["1", 2, "3"]), ['2']); // => true;
+assertArraysEqual(without(["1", "1", "1", "2", "3", "1" , "1"], ["1", "2", 3]), ['3']); // => false;
 
 assertArraysEqual((without([1, 2, 3], [1])), [2,3]); // => True
 assertArraysEqual((without(["1", "2", "3"], [1, 2, "3"])), [1, 2]); // => False
