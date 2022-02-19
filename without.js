2ecda63df1f6c3f6d5d8e30f606d70f4d8471a15
Matt <mgstankov30@gmail.com>
2022-02-18 23:24:31 -0500

    refactor the code to accomidate newer loops and practises to ensure it meets all requirements for the lotide assignment

--- without.js
+++ without.js
@@ -1,34 +1,27 @@
 // need to credit @GideonBrasil for his excellent help and patiences as a mentor, He did a great job explaining what we were doing as I worked through the code.
-const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
 
 //Implement without which will return a subset of a given array, removing unwanted elements. --- done
 //This function should take in a source array and a itemsToRemove array. --- done
 //It should return a new array  --- done
 //New Array should only have those elements from the source that are not present in the itemsToRemove array. --- done
 
-const without = (arr1, itemsToRemove) => {
-  // starting with a blank array
+//refactoring code to work easier.
+
+const without = function (arr1, itemsToRemove) {
+  // setting up a new array
   let newArr = [];
-  // we do a simple for loop on the array being input in the function param.
-  for (let y = 0; y < arr1.length; y++) {
-    // we can then push the y index from the input array to the blank array.
-    newArr.push(arr1 [y]);
-  }
-  //at this point we then need to loop through the values of our newArr as well as the items to remove.
-  for (let i = 0; i < newArr.length; i++) {
-    // this looks at the new array looping through each element.
-    for (let j = 0; j < itemsToRemove.length; j++) {
-      // this looks at the second array of items to remove.
-      if (newArr[i] === itemsToRemove[j]) {
-        // since we are doing a loop of both by the values of the items to remove we can say when they match/
-        // splice from the newArr that index and DON'T add anything back (hence the ([i], 1)).
-        newArr.splice([i], 1);
-      }
+  //filtering through with the lovely for of loop of each element of the first array.
+  for (const item of arr1) {
+    // if the items to remove doesn't include the element from arr1
+    if (!itemsToRemove.includes(item)) {
+      // push the element to the new array.
+      newArr.push(item);
     }
   }
-  // we then retunr the NewArr that was spliced and are therefore not affecting the original array input so we can use the assert Arrays equal as well.
+  // return false
   return newArr;
 };
+
+
 
 module.exports = without;
