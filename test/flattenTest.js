6d365f2384f4be1e78ac70d0077ca84652276040
Matt <mgstankov30@gmail.com>
2022-02-18 22:34:09 -0500

    updated Flatten test to better showcase with Mocha and chai

--- test/flattenTest.js
+++ test/flattenTest.js
@@ -1,5 +1,21 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const flatten = require('../flatten');
 
-flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); //true
+const flatten = require('../flatten');
+const assert = require('chai').assert;
+
+describe('#Flatten', () => {
+  it("returns [1, 2, 3, 4, 5, 6] for [1, 2, [3,4], 5, [6]]", () => {
+    assert.deepEqual(flatten([1, 2, [3,4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+  });
+  it("returns [1, 2, 3, 4, 5, 6, 7, 8] for [1, [2], [3,4], 5, [6, 7, [8]]]", () => {
+    assert.deepEqual(flatten([1, [2], [3,4], 5, [6, 7], [8]]), [1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+  it("returns ['Lighthouse', 'Labs', 'Hello'] for ['Lighthouse', ['Labs', 'Hello']]", () => {
+    assert.deepEqual(flatten(['Lighthouse', ['Labs', 'Hello']]), ['Lighthouse', 'Labs', 'Hello']);
+  });
+  it("returns ['John', 'Jacob', 'Smith'] for [['John'],['Jacob'],['Smith']]", () => {
+    assert.deepEqual(flatten([['John'],['Jacob'],['Smith']]), ['John', 'Jacob', 'Smith']);
+  });
+  it("returns [1, 2, 3, 4, 5, 6] for [1, 2, [3,4], 5, [6]]", () => {
+    assert.notDeepEqual(flatten([1, 2, [3,4], 5, [6]]), [1, 2, 3, 4, [5], [6]]);
+  });
+});
